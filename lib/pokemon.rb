c7d95e4019b5baa78a72b71ac9d2053a125b47e0
Anthony Tilelli <anthony@tilelli.me>
2020-01-04 02:01:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,17 @@ class Pokemon
   end
   
   def save
-    
-  end
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO pokemon (name, grade) 
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   
 end
