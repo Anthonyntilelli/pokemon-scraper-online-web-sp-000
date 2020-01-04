282b20d86f730faf83c7e33020403ceb93797369
Anthony Tilelli <anthony@tilelli.me>
2020-01-04 01:50:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,12 @@
 class Pokemon
+
+  attr_accessor :name, :type, :id
   
-  def initialized(:name,:type,id:nil)
+  def initialized(:name,:type, id:nil)
+    @name=name
+    @type=type
+    @id=id
+    
   end
   
 end
